2d096083cad94f236d32ad2e18808101fea75be2
Bo-Lennart Heitmann <bo.heitmann@gmail.com>
2022-11-02 09:48:59 +0000

    continue to new question

--- run.py
+++ run.py
@@ -10,4 +10,8 @@ for question in question_data:
     question_bank.append(new_question)
 
 quiz = QuizGame(question_bank)
+
+while quiz.still_has_question():
+    quiz.next_question()
+
 quiz.next_question()
