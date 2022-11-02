1800bf586934b357433a4860d93f8d76370cb7b4
Bo-Lennart Heitmann <bo.heitmann@gmail.com>
2022-11-02 09:31:27 +0000

    add question and input for user to answer

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from question_model import Question
 from data import question_data
+from quiz_game import QuizGame
 
 question_bank = []
 for question in question_data:
@@ -8,4 +9,5 @@ for question in question_data:
     new_question = Question(question_text,question_answer)
     question_bank.append(new_question)
 
-print(question_bank)
+quiz = QuizGame(question_bank)
+quiz.next_question()
